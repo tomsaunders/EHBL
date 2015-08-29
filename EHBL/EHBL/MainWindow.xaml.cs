9af9a57022b8ccdc579feb45173d3c662d2b5d1e
Tom Saunders <tom@tsaunders.net>
2015-08-29 15:10:44 +1000

    initial commit of WPF project. Only does some basic placeholder responses to menu clicks

---
+++ EHBL/EHBL/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EHBL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            helpLabel.Visibility = Visibility.Hidden;
+            optionsLabel.Visibility = Visibility.Hidden;
+            gamesLabel.Visibility = Visibility.Hidden;
+        }   
+
+        private void Games_Click(object sender, RoutedEventArgs e)
+        {
+            helpLabel.Visibility = Visibility.Hidden;
+            optionsLabel.Visibility = Visibility.Hidden;
+            gamesLabel.Visibility = Visibility.Visible;
+        }
+
+        private void Options_Click(object sender, RoutedEventArgs e)
+        {
+            helpLabel.Visibility = Visibility.Hidden;
+            optionsLabel.Visibility = Visibility.Visible;
+            gamesLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            helpLabel.Visibility = Visibility.Visible;
+            optionsLabel.Visibility = Visibility.Hidden;
+            gamesLabel.Visibility = Visibility.Hidden;
+        }
+    }
+}
